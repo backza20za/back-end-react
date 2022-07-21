a99e100ed38a065a7915d5f66eba417a7b5da2f0
Back <back4936nantawat@gmail.com>
2022-07-21 22:56:17 +0700

    testcommit

--- database/db.confic.js
+++ database/db.confic.js
@@ -1,14 +1,14 @@
 module.exports = {
-    HOST:"localhost",
-    USER:"root",
-    PASSWORD:"",
-    DB:"Back-End_fewfans",
-    dialect:"mysql",
-    timezone:"asia/bangkok",
-    pool:{
-        max:5,
-        min:0,
-        acquire:30000,
-        idle:10000
-    }
-}
+  HOST: "localhost",
+  USER: "root",
+  PASSWORD: "",
+  DB: "Back-End_fewfans",
+  dialect: "mysql",
+  timezone: "asia/bangkok",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 1000,
+  },
+};
